ce08e86e1f74192278576fa89f6d6ed9b89140a7
ntuan <ntdcap1@gmail.com>
2020-06-28 15:32:20 +0700

    modifed

--- wallet/debugtool/main/main.go
+++ wallet/debugtool/main/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/incognitochain/incognito-chain/privacy/debugtool"
 	"github.com/incognitochain/incognito-chain/wallet"
+	"github.com/incwallet/wallet/debugtool"
 	"os"
 	"strconv"
 )
